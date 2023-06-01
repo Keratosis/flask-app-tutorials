2e54f099e06e5f3517fa056ac7962448998eb7a3
braxtonshaffie <braxtonshaffie@gmail.com>
2023-06-02 02:47:45 +0300

    creating flask application

---
+++ website/__init__.py
@@ -0,0 +1,8 @@
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__) #, it represents the name of the file in which this code is written
+    app.config['SECRET_KEY'] = 'youguysaregettingpaid'
+    
+    return app
+    
