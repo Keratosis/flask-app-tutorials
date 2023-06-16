86778b46bda67e176f40798e3b54d0d8743d3f76
braxtonshaffie <braxtonshaffie@gmail.com>
2023-06-16 09:35:57 +0300

    setting sqlachemy

--- website/__init__.py
+++ website/__init__.py
@@ -1,14 +1,21 @@
 
 #placing the __init__ in a folder makes it  a python package  
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
 
 def create_app():
     app = Flask(__name__) #, it represents the name of the file in which this code is written
     app.config['SECRET_KEY'] = 'youguysaregettingpaid' #secure the cookies in website
+    app.config['SQLALCHEMY_DATABASE_URL'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app) #initialising database with app
     
     from .view import view 
     from .auth import auth
-    
+      
     
     app.register_blueprint(view, url_prefix ="/")
     app.register_blueprint(auth, url_prefix ="/")
