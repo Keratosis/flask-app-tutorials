d8fbf940dadb7fe4e8eb70ed6eb8847a5ec514e3
braxtonshaffie <braxtonshaffie@gmail.com>
2023-06-16 16:52:43 +0300

    creating database

--- website/__init__.py
+++ website/__init__.py
@@ -2,15 +2,16 @@
 #placing the __init__ in a folder makes it  a python package  
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import path # importing path
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
 
-def create_app():
+def create_app():  
     app = Flask(__name__) #, it represents the name of the file in which this code is written
     app.config['SECRET_KEY'] = 'youguysaregettingpaid' #secure the cookies in website
-    app.config['SQLALCHEMY_DATABASE_URL'] = f'sqlite:///{DB_NAME}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app) #initialising database with app
     
     from .view import view 
@@ -19,7 +20,17 @@ def create_app():
     
     app.register_blueprint(view, url_prefix ="/")
     app.register_blueprint(auth, url_prefix ="/")
-     
+    
+    from .model import User
+    create_database(app)
      
     return app
+
+def create_database(app):
+    #create database if it doesnot exist
+    if not path.exists('website/'+ DB_NAME):
+        with app.app_context():
+            db.create_all()
+        
+        print('Database created!')
     
